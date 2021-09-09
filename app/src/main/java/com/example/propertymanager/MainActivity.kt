cb60523c65576aaebf279cce625ab86eb8ee486c
aschimmichanga <ashnasrivastava1@gmail.com>
2021-09-09 16:37:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/propertymanager/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.propertymanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.propertymanager.ui.theme.PropertyManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PropertyManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PropertyManagerTheme {
+        Greeting("Android")
+    }
+}
